c32089484b913100829acd3cd28a884a15c61ac4
Malcolm Peter <kraftycoder@gmail.com>
2018-09-11 14:24:50 +0300

    add quesions fetcher api

--- js/script.js
+++ js/script.js
@@ -11,9 +11,21 @@ window.onload = function home(){
     .then((data)=>{
       document.getElementById("output").innerHTML = data["message"]
     })
-  .catch((err) => console.log(err))
+  .catch((err) => console.log(err));
 };
 
-
+window.onload = function questions() {
+  fetch("https://s-over-lite.herokuapp.com/api/v1/questions", {
+    method: "GET",
+    headers:{
+      "Content-Type":"application/json"
+    }
+  })
+  .then((response) => response.json())
+    .then((data)=>{
+      document.getElementById("questions").innerHTML = data["message"]
+    })
+  .catch((err) => console.log(err));
+}
 
 
